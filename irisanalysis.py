297e51b32b7a0641df52507acdb21e8554a8fcf3
Kennymctigue <36159017+Kennymctigue@users.noreply.github.com>
2018-04-29 21:55:49 +0100

    Update irisanalysis.py
    To adjust code after realising I had got the the petal and sepal columns mixed up.... Jeesh!!

--- irisanalysis.py
+++ irisanalysis.py
@@ -8,13 +8,13 @@
 # https://matplotlib.org/
 # https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
 
-import numpy
+import numpy # http://www.numpy.org/
 
 
 
 data = numpy.genfromtxt("data/iris.csv", delimiter = ",") 
 
-import matplotlib.pyplot as pl 
+import matplotlib.pyplot as pl # https://matplotlib.org/
 
 
 # Four variables, Petal length, Petal width, Sepal length and Sepal width
@@ -24,113 +24,113 @@ set = "Setosa"
 ver = "Versicolor"
 vir = "Virginica"
 
-petlen = data[:,0] # first column
-petwid = data[:,1] # second column
-seplen = data[:,2] # third column
-sepwid = data[:,3] # fourth Column
+petlen = data[:,2] # first column petal length
+petwid = data[:,3] # second column petal width
+seplen = data[:,0] # third column sepal length
+sepwid = data[:,1] # fourth Column sepal width
 
 
 # to calculate the maximum of each variable
-petlenmax = numpy.amax(data[:,0])
-petwidmax = numpy.amax(data[:,1])
-seplenmax = numpy.amax(data[:,2])
-sepwidmax = numpy.amax(data[:,3])
+petlenmax = numpy.amax(data[:,2])
+petwidmax = numpy.amax(data[:,3])
+seplenmax = numpy.amax(data[:,0])
+sepwidmax = numpy.amax(data[:,1])
 
 # to calculate the minimum of each variable
-petlenmin = numpy.amin(data[:,0])
-petwidmin = numpy.amin(data[:,1])
-seplenmin = numpy.amin(data[:,2])
-sepwidmin = numpy.amin(data[:,3])
+petlenmin = numpy.amin(data[:,2])
+petwidmin = numpy.amin(data[:,3])
+seplenmin = numpy.amin(data[:,0])
+sepwidmin = numpy.amin(data[:,1])
 
 # To calculate the mean of each variable
-petlenmean = round(numpy.mean(data[:,0]),2)
-petwidmean = round(numpy.mean(data[:,1]),2)
-seplenmean = round(numpy.mean(data[:,2]),2)
-sepwidmean = round(numpy.mean(data[:,3]),2)
+petlenmean = round(numpy.mean(data[:,2]),2)
+petwidmean = round(numpy.mean(data[:,3]),2)
+seplenmean = round(numpy.mean(data[:,0]),2)
+sepwidmean = round(numpy.mean(data[:,1]),2)
 
 # To calculate the standard deviation from the mean of each variable
-petlenstd = round(numpy.std(data[:,0]),3)
-petwidstd = round(numpy.std(data[:,1]),3)
-seplenstd = round(numpy.std(data[:,2]),3)
-sepwidstd = round(numpy.std(data[:,3]),3)
+petlenstd = round(numpy.std(data[:,2]),3)
+petwidstd = round(numpy.std(data[:,3]),3)
+seplenstd = round(numpy.std(data[:,0]),3)
+sepwidstd = round(numpy.std(data[:,1]),3)
 
 # To identify the data of just the Setosa class of Iris
-setpetlen = data[1:50,0]
-setpetwid = data[1:50,1]
-setseplen = data[1:50,2]
-setsepwid = data[1:50,3] 
+setpetlen = data[1:50,2]
+setpetwid = data[1:50,3]
+setseplen = data[1:50,0]
+setsepwid = data[1:50,1] 
 
 # To calculate the maximum of each variable in the Setosa class of Iris
-setpetlenmax = numpy.amax(data[1:50,0])
-setpetwidmax = numpy.amax(data[1:50,1])
-setseplenmax = numpy.amax(data[1:50,2])
-setsepwidmax = numpy.amax(data[1:50,3])
+setpetlenmax = numpy.amax(data[1:50,2])
+setpetwidmax = numpy.amax(data[1:50,3])
+setseplenmax = numpy.amax(data[1:50,0])
+setsepwidmax = numpy.amax(data[1:50,1])
 
 # To calculate the minimum of each variable in the Setosa class of Iris
-setpetlenmin = numpy.amin(data[1:50,0])
-setpetwidmin = numpy.amin(data[1:50,1])
-setseplenmin = numpy.amin(data[1:50,2])
-setsepwidmin = numpy.amin(data[1:50,3])
+setpetlenmin = numpy.amin(data[1:50,2])
+setpetwidmin = numpy.amin(data[1:50,3])
+setseplenmin = numpy.amin(data[1:50,0])
+setsepwidmin = numpy.amin(data[1:50,1])
 
 # To calculate the mean of each variable for the Setosa class of Iris
-setpetlenmean = numpy.mean(data[1:50,0])
-setpetwidmean = numpy.mean(data[1:50,1])
-setseplenmean = numpy.mean(data[1:50,2])
-setsepwidmean = numpy.mean(data[1:50,3])
+setpetlenmean = numpy.mean(data[1:50,2])
+setpetwidmean = numpy.mean(data[1:50,3])
+setseplenmean = numpy.mean(data[1:50,0])
+setsepwidmean = numpy.mean(data[1:50,1])
 
 # To identify the data of just the Versicolor class of Iris 
-verpetlen = data[51:100,0]
-verpetwid = data[51:100,1]
-verseplen = data[51:100,2]
-versepwid = data[51:100,3]
+verpetlen = data[51:100,2]
+verpetwid = data[51:100,3]
+verseplen = data[51:100,0]
+versepwid = data[51:100,1]
 
 # To calculate the maximum of each variable in the Versicolor class of Iris
-verpetlenmax = numpy.amax(data[51:100,0])
-verpetwidmax = numpy.amax(data[51:100,1])
-verseplenmax = numpy.amax(data[51:100,2])
-versepwidmax = numpy.amax(data[51:100,3])
+verpetlenmax = numpy.amax(data[51:100,2])
+verpetwidmax = numpy.amax(data[51:100,3])
+verseplenmax = numpy.amax(data[51:100,0])
+versepwidmax = numpy.amax(data[51:100,1])
 
 # To calculate the minimum of each variable in the Versicolor class of Iris
-verpetlenmin = numpy.amin(data[51:100,0])
-verpetwidmin = numpy.amin(data[51:100,1])
-verseplenmin = numpy.amin(data[51:100,2])
-versepwidmin = numpy.amin(data[51:100,3])
+verpetlenmin = numpy.amin(data[51:100,2])
+verpetwidmin = numpy.amin(data[51:100,3])
+verseplenmin = numpy.amin(data[51:100,0])
+versepwidmin = numpy.amin(data[51:100,1])
 
 # To calculate the mean of each variable for the Versicolor class of Iris
-verpetlenmean = numpy.mean(data[51:100,0])
-verpetwidmean = numpy.mean(data[51:100,1])
-verseplenmean = numpy.mean(data[51:100,2])
-versepwidmean = numpy.mean(data[51:100,3])
+verpetlenmean = numpy.mean(data[51:100,2])
+verpetwidmean = numpy.mean(data[51:100,3])
+verseplenmean = numpy.mean(data[51:100,0])
+versepwidmean = numpy.mean(data[51:100,1])
 
 # To identify the data of just the Virginica class of Iris
-virpetlen = data[101:150,0]
-virpetwid = data[101:150,0]
-virseplen = data[101:150,2]
-virsepwid = data[101:150,3]
+virpetlen = data[101:150,2]
+virpetwid = data[101:150,3]
+virseplen = data[101:150,0]
+virsepwid = data[101:150,1]
 
 # To calculate the maximum of each variable in the Virginica class of Iris
-virpetlenmax = numpy.amax(data[101:150,0])
-virpetwidmax = numpy.amax(data[101:150,1])
-virseplenmax = numpy.amax(data[101:150,2])
-virsepwidmax = numpy.amax(data[101:150,3])
+virpetlenmax = numpy.amax(data[101:150,2])
+virpetwidmax = numpy.amax(data[101:150,3])
+virseplenmax = numpy.amax(data[101:150,0])
+virsepwidmax = numpy.amax(data[101:150,1])
 
 # To calculate the minimum of each variable in the Virginica class of Iris
-virpetlenmin = numpy.amin(data[101:150,0])
-virpetwidmin = numpy.amin(data[101:150,1])
-virseplenmin = numpy.amin(data[101:150,2])
-virsepwidmin = numpy.amin(data[101:150,3])
+virpetlenmin = numpy.amin(data[101:150,2])
+virpetwidmin = numpy.amin(data[101:150,3])
+virseplenmin = numpy.amin(data[101:150,0])
+virsepwidmin = numpy.amin(data[101:150,1])
 
 # To calculate the mean of each variable for the Virginica class of Iris
-virpetlenmean = numpy.mean(data[101:150,0])
-virpetwidmean = numpy.mean(data[101:150,1])
-virseplenmean = numpy.mean(data[101:150,2])
-virsepwidmean = numpy.mean(data[101:150,3])
+virpetlenmean = numpy.mean(data[101:150,2])
+virpetwidmean = numpy.mean(data[101:150,3])
+virseplenmean = numpy.mean(data[101:150,0])
+virsepwidmean = numpy.mean(data[101:150,1])
 
 # To calculate the standard deviation from the mean for the Virginica class of Iris
-virpetlenstd = numpy.std(data[101:150,0])
-virpetwidstd = numpy.std(data[101:150,1])
-virseplenstd = numpy.std(data[101:150,2])
-virsepwidstd = numpy.std(data[101:150,3])
+virpetlenstd = numpy.std(data[101:150,2])
+virpetwidstd = numpy.std(data[101:150,3])
+virseplenstd = numpy.std(data[101:150,0])
+virsepwidstd = numpy.std(data[101:150,1])
 
 
 if setpetlenmax > verpetlenmax and setpetlenmax > virpetlenmax:
@@ -192,4 +192,3 @@ elif versepwidmin < setsepwidmin and versepwidmin < virsepwidmin:
 else:
     print ("The", vir , "class has the lowest sepal  minimum width with", virsepwidmin, "cm")
 print ("The average sepal width across all the classes is", sepwidmean, "cm with a standard deviation of", sepwidstd)
-
